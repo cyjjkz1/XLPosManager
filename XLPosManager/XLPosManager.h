8d4fc708f8f1b3daf275a7b9e72388eec28998af
mijiahao <ttmijiahao@sina.com>
2019-10-10 15:42:51 +0800

    Initial Commit

---
+++ XLPosManager/XLPosManager.h
@@ -0,0 +1,13 @@
+//
+//  XLPosManager.h
+//  XLPosManager
+//
+//  Created by heting on 2019/10/10.
+//  Copyright © 2019年 ccd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XLPosManager : NSObject
+
+@end
---
+++ XLPosManager/XLPosManager.m
@@ -0,0 +1,13 @@
+//
+//  XLPosManager.m
+//  XLPosManager
+//
+//  Created by heting on 2019/10/10.
+//  Copyright © 2019年 ccd. All rights reserved.
+//
+
+#import "XLPosManager.h"
+
+@implementation XLPosManager
+
+@end
